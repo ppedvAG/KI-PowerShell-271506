a13d077186ceeb55e8cb23b3fcfbbc4c17ad4b12
Stefan Ober <StefanO@ppedv.de>
2026-09-21 12:04:57 +0200

    Add Get-SecLog.ps1 script for event log retrieval and update README with course agenda

---
+++ Beispiele/Get-SecLog.ps1
@@ -0,0 +1,27 @@
+﻿<#
+.SYNOPSIS 
+ Kurzbeschreibung welche Aufgaben
+.DESCRIPTION
+ Eine lange Beschreibung
+.PARAMETER EventId
+ 4624 Anmeldung
+ 4625 fehlgeschlagene Anmeldung
+ 4634 Abmeldung
+.LINK
+ https://learn.microsoft.com/en-us/powershell/module/microsoft.powershell.core/about/about_comment_based_help?view=powershell-5.1#syntax-for-comment-based-help-in-functions
+#>
+[cmdletBinding()]
+param(
+[Parameter(Mandatory=$true)]
+[ValidateSet(4624,4625,4634)]
+[int]$EventId,
+
+[ValidateRange(5,20)]
+[int]$Newest = 5,
+
+[string]$Computername = "localhost"
+)
+Write-Verbose -Message "Optionale Ausgabe die nur ausgegeben wird wenn das SKript mit -Verbose gestartet wird"
+Write-Debug -Message "Debughaltepunkte die nur wirken wenn das Skript mit -Debug aufgerufen "
+Get-EventLog -LogName Security -ComputerName $Computername | Where-Object -FilterScript {$PSItem.EventID -eq $EventId} | Select-Object -First $Newest
+
--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # KI-PowerShell-271506
-KursRepository zu Kurs PowerShell mit AI – Automatisierung für Clevere der ppedv AG
+KursRepository zu Kurs PowerShell mit AI – Automatisierung für Clevere der ppedv
+
+#Agenda
+- Begrüßung
+- Crashkurs
+    - PowerShell Grundlagen
+    - Pipeline
+    - Variablen
+    - cmdlets
+    - Skriptsicherheit
+    - Scripting
+        - Parameter
+        - Comment Based Help
+        - Verbose
+        - Debug
+        
